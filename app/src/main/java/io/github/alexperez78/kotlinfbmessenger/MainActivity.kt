529cc8b3a604adefb80a710aa2a270f753ecc808
Alex Perez <alex_perez25@hotmail.com>
2018-07-31 20:19:49 -0700

    Adding Basic Layout
    Nothing pretty yet, simple connections and layout additions

--- app/src/main/java/io/github/alexperez78/kotlinfbmessenger/MainActivity.kt
+++ app/src/main/java/io/github/alexperez78/kotlinfbmessenger/MainActivity.kt
@@ -1,12 +1,32 @@
 package io.github.alexperez78.kotlinfbmessenger
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        register_button_register.setOnClickListener {
+            val email = email_editText_registration.text.toString()
+            val password = password_editText_registration.text.toString()
+
+            Log.d("MainActivity", "Email is " + email)
+            Log.d("MainActivity", "Password is: $password")
+
+            //Firebase Authetication to create a user with credentials
+        }
+
+        already_have_an_account_textView.setOnClickListener {
+            Log.d("MainActivity", "Its not yet created")
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
