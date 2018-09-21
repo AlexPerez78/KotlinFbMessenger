5f65d1370db53e459970e45094d8344e95ffe63d
Alex Perez <alex_perez25@hotmail.com>
2018-09-21 16:57:26 -0700

    FIrebase Connection + UI Fixes
    1) FIrebase works for login and register
    2) Ui fix, removed box to rounded corners

--- app/src/main/java/io/github/alexperez78/kotlinfbmessenger/LoginActivity.kt
+++ app/src/main/java/io/github/alexperez78/kotlinfbmessenger/LoginActivity.kt
@@ -2,11 +2,25 @@ package io.github.alexperez78.kotlinfbmessenger
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_login)
+
+        login_button_login.setOnClickListener {
+            val email = email_textView_login.text.toString()
+            val password = password_textView_login.text.toString()
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+        }
+
+        back_to_register_textView.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/io/github/alexperez78/kotlinfbmessenger/MainActivity.kt
+++ app/src/main/java/io/github/alexperez78/kotlinfbmessenger/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,13 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         register_button_register.setOnClickListener {
-            val email = email_editText_registration.text.toString()
-            val password = password_editText_registration.text.toString()
-
-            Log.d("MainActivity", "Email is " + email)
-            Log.d("MainActivity", "Password is: $password")
-
-            //Firebase Authetication to create a user with credentials
+            performRegister()
         }
 
         already_have_an_account_textView.setOnClickListener {
@@ -28,5 +24,30 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    private fun performRegister(){
+        val email = email_editText_registration.text.toString()
+        val password = password_editText_registration.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Username or Password is empty, Please enter the fields.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d("MainActivity", "Email is " + email)
+        Log.d("MainActivity", "Password is: $password")
+
+        //Firebase Authetication to create a user with credentials
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener{
+                    if(!it.isSuccessful) return@addOnCompleteListener
+
+                    Log.d("Main","Successful created user with uid: ${it.result.user.uid}")
+                }
+                .addOnFailureListener{
+                    Log.d("Main", "Failed to create user: ${it.message}")
+                    Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT).show()
+                }
     }
 }
